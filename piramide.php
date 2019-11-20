f9642690f6a7e023ca1afda6c609c1d6674ed628
Stijn van_der_Made <2120804@talnet.nl>
2019-11-20 13:54:40 +0100

    Uitwerking van de exercise

--- piramide.php
+++ piramide.php
@@ -1,5 +1,7 @@
 <?php
-for ($i = 1; $i < 5; $i++) {
+echo("hoeveel stapels? ");
+$stapels = readline();
+for ($i = 1; $i < $stapels+1; $i++) {
     for ($j = 1; $j <= $i; $j++) {
         echo " * ";
     }
